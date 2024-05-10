cc1459261ef651915e20fb35c59641c4fd88bcf4
Jack <5182053+phyzical@users.noreply.github.com>
2024-05-10 23:15:43 +0800

    Avoid removing completed items during slow checks
    Add logic to avoid removing items that have been finished during slow check.
    
    For example sab might be unpacking lots of items which declutter detects as "slow downloads"

--- src/jobs/remove_slow.py
+++ src/jobs/remove_slow.py
@@ -15,7 +15,7 @@ async def remove_slow(settingsDict, BASE_URL, API_KEY, NAME, deleted_downloads, 
         alreadyCheckedDownloadIDs = []
         for queueItem in queue['records']:
             if 'downloadId' in queueItem and 'size' in queueItem and 'sizeleft' in queueItem and 'status' in queueItem:
-                if queueItem['downloadId'] not in alreadyCheckedDownloadIDs:
+                if queueItem['downloadId'] not in alreadyCheckedDownloadIDs and queueItem['sizeleft'] > 0:
                     alreadyCheckedDownloadIDs.append(queueItem['downloadId']) # One downloadId may occur in multiple queueItems - only check once for all of them per iteration
                     # determine if the downloaded bit on average between this and the last iteration is greater than the min threshold
                     downloadedSize, previousSize, increment, speed = await getDownloadedSize(settingsDict, queueItem, download_sizes_tracker, NAME)
