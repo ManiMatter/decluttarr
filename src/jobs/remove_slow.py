4ec4cd48dfbc3be69ee3a7516a7a1e61076be770
phyzical <5182053+phyzical@users.noreply.github.com>
2024-05-11 01:02:00 +0800

    remove redundant comment

--- src/jobs/remove_slow.py
+++ src/jobs/remove_slow.py
@@ -15,7 +15,6 @@ async def remove_slow(settingsDict, BASE_URL, API_KEY, NAME, deleted_downloads, 
         alreadyCheckedDownloadIDs = []
         for queueItem in queue['records']:
             if 'downloadId' in queueItem and 'size' in queueItem and 'sizeleft' in queueItem and 'status' in queueItem and 'protocol' in queueItem:
-                #  either size left isn't 0 OR it is 0 and so issize (a torrent/download that never started)
                 if queueItem['downloadId'] not in alreadyCheckedDownloadIDs and queueItem['protocol'] == 'torrent':
                     alreadyCheckedDownloadIDs.append(queueItem['downloadId']) # One downloadId may occur in multiple queueItems - only check once for all of them per iteration
                     # determine if the downloaded bit on average between this and the last iteration is greater than the min threshold
