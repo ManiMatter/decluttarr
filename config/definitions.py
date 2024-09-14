c95622677475dc8fcf82a7177b49995fbf15d4b4
Benjamin Harder <benjamin.harder@gmail.com>
2024-09-14 10:57:20 +0200

    Ignoring black for definitions file

--- config/definitions.py
+++ config/definitions.py
@@ -25,88 +25,61 @@ IGNORE_PRIVATE_TRACKERS         = get_config_value('IGNORE_PRIVATE_TRACKERS',   
 FAILED_IMPORT_MESSAGE_PATTERNS  = get_config_value('FAILED_IMPORT_MESSAGE_PATTERNS','features',     False,  list,   [])
 
 # Radarr
-RADARR_URL = get_config_value("RADARR_URL", "radarr", False, str)
-RADARR_KEY = (
-    None if RADARR_URL == None else get_config_value("RADARR_KEY", "radarr", True, str)
-)
+RADARR_URL                      = get_config_value('RADARR_URL',                    'radarr',       False,  str)
+RADARR_KEY                      = None if RADARR_URL == None else \
+                                  get_config_value('RADARR_KEY',                    'radarr',       True,   str)
 
-# Sonarr
-SONARR_URL = get_config_value("SONARR_URL", "sonarr", False, str)
-SONARR_KEY = (
-    None if SONARR_URL == None else get_config_value("SONARR_KEY", "sonarr", True, str)
-)
+# Sonarr        
+SONARR_URL                      = get_config_value('SONARR_URL',                    'sonarr',       False,  str)
+SONARR_KEY                      = None if SONARR_URL == None else \
+                                  get_config_value('SONARR_KEY',                    'sonarr',       True,   str)
 
-# Lidarr
-LIDARR_URL = get_config_value("LIDARR_URL", "lidarr", False, str)
-LIDARR_KEY = (
-    None if LIDARR_URL == None else get_config_value("LIDARR_KEY", "lidarr", True, str)
-)
+# Lidarr        
+LIDARR_URL                      = get_config_value('LIDARR_URL',                    'lidarr',       False,  str)
+LIDARR_KEY                      = None if LIDARR_URL == None else \
+                                  get_config_value('LIDARR_KEY',                    'lidarr',       True,   str)
 
-# Readarr
-READARR_URL = get_config_value("READARR_URL", "readarr", False, str)
-READARR_KEY = (
-    None
-    if READARR_URL == None
-    else get_config_value("READARR_KEY", "readarr", True, str)
-)
+# Readarr       
+READARR_URL                     = get_config_value('READARR_URL',                   'readarr',       False,  str)
+READARR_KEY                     = None if READARR_URL == None else \
+                                  get_config_value('READARR_KEY',                   'readarr',       True,   str)
 
-# Whisparr
-WHISPARR_URL = get_config_value("WHISPARR_URL", "whisparr", False, str)
-WHISPARR_KEY = (
-    None
-    if WHISPARR_URL == None
-    else get_config_value("WHISPARR_KEY", "whisparr", True, str)
-)
+# Whisparr    
+WHISPARR_URL                    = get_config_value('WHISPARR_URL',                  'whisparr',       False,  str)
+WHISPARR_KEY                    = None if WHISPARR_URL == None else \
+                                  get_config_value('WHISPARR_KEY',                  'whisparr',       True,   str)
 
-# qBittorrent
-QBITTORRENT_URL = get_config_value("QBITTORRENT_URL", "qbittorrent", False, str, "")
-QBITTORRENT_USERNAME = get_config_value(
-    "QBITTORRENT_USERNAME", "qbittorrent", False, str, ""
-)
-QBITTORRENT_PASSWORD = get_config_value(
-    "QBITTORRENT_PASSWORD", "qbittorrent", False, str, ""
-)
+# qBittorrent   
+QBITTORRENT_URL                 = get_config_value('QBITTORRENT_URL',               'qbittorrent',  False,  str,    '')
+QBITTORRENT_USERNAME            = get_config_value('QBITTORRENT_USERNAME',          'qbittorrent',  False,  str,    '')
+QBITTORRENT_PASSWORD            = get_config_value('QBITTORRENT_PASSWORD',          'qbittorrent',  False,  str,    '')
 
 ########################################################################################################################
 ########### Validate settings
-if not (
-    IS_IN_PYTEST
-    or RADARR_URL
-    or SONARR_URL
-    or LIDARR_URL
-    or READARR_URL
-    or WHISPARR_URL
-):
-    print(
-        f"[ ERROR ]: No Radarr/Sonarr/Lidarr/Readarr/Whisparr URLs specified (nothing to monitor)"
-    )
+if not (IS_IN_PYTEST or RADARR_URL or SONARR_URL or LIDARR_URL or READARR_URL or WHISPARR_URL):
+    print(f'[ ERROR ]: No Radarr/Sonarr/Lidarr/Readarr/Whisparr URLs specified (nothing to monitor)')
     exit()
 
 ########### Enrich setting variables
-if RADARR_URL:
-    RADARR_URL = RADARR_URL.rstrip("/") + "/api/v3"
-if SONARR_URL:
-    SONARR_URL = SONARR_URL.rstrip("/") + "/api/v3"
-if LIDARR_URL:
-    LIDARR_URL = LIDARR_URL.rstrip("/") + "/api/v1"
-if READARR_URL:
-    READARR_URL = READARR_URL.rstrip("/") + "/api/v1"
-if WHISPARR_URL:
-    WHISPARR_URL = WHISPARR_URL.rstrip("/") + "/api/v3"
-if QBITTORRENT_URL:
-    QBITTORRENT_URL = QBITTORRENT_URL.rstrip("/") + "/api/v2"
+if RADARR_URL:      RADARR_URL =        RADARR_URL.rstrip('/')      + '/api/v3'
+if SONARR_URL:      SONARR_URL =        SONARR_URL.rstrip('/')      + '/api/v3'
+if LIDARR_URL:      LIDARR_URL =        LIDARR_URL.rstrip('/')      + '/api/v1'
+if READARR_URL:     READARR_URL =       READARR_URL.rstrip('/')     + '/api/v1'
+if WHISPARR_URL:    WHISPARR_URL =      WHISPARR_URL.rstrip('/')    + '/api/v3'
+if QBITTORRENT_URL: QBITTORRENT_URL =   QBITTORRENT_URL.rstrip('/') + '/api/v2'
 
-RADARR_MIN_VERSION = "5.3.6.8608"
-SONARR_MIN_VERSION = "4.0.1.1131"
-LIDARR_MIN_VERSION = None
-READARR_MIN_VERSION = None
-WHISPARR_MIN_VERSION = "2.0.0.548"
-QBITTORRENT_MIN_VERSION = "4.3.0"
+RADARR_MIN_VERSION          = '5.3.6.8608'
+SONARR_MIN_VERSION          = '4.0.1.1131'
+LIDARR_MIN_VERSION          = None
+READARR_MIN_VERSION         = None
+WHISPARR_MIN_VERSION        = '2.0.0.548'
+QBITTORRENT_MIN_VERSION     = '4.3.0'
 
-SUPPORTED_ARR_APPS = ["RADARR", "SONARR", "LIDARR", "READARR", "WHISPARR"]
+SUPPORTED_ARR_APPS  = ['RADARR', 'SONARR', 'LIDARR', 'READARR', 'WHISPARR']
 
 ########### Add Variables to Dictionary
 settingsDict = {}
 for var_name in dir():
     if var_name.isupper():
         settingsDict[var_name] = locals()[var_name]
+
